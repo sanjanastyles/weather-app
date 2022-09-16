b7ff5bfea2a6b6cdc10177a9dd993304c81e60b0
KANATA <sanjana2842002@gmail.com>
2022-09-16 14:12:40 +0530

    .

---
+++ README.md
@@ -0,0 +1,6 @@
+## Dependencies
+
+1. [tailwind](https://tailwindcss.com/docs/guides/create-react-app)
+2. [Unicons](https://github.com/Iconscout/react-unicons)
+3. [Toastify](https://www.npmjs.com/package/react-toastify)
+4. [Luxon](https://www.npmjs.com/package/luxon)
---
+++ src/components/TopButtons.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
+    },
+    {
+      id: 1,
+      title: "Mumbai",
+    },
+    {
+      id: 1,
+      title: "Sydney",
+    },
+    {
+      id: 1,
+      title: "New York",
+    },
+    {
+      id: 5,
+      title: "Tokyo",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
